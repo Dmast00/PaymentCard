83e69d0eff5c8d969bf8f9986ff583ab7393ab15
dmast00 <obed.hinojosa@outlook.com>
2022-01-28 15:03:59 -0600

    not showing data

--- src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from 'src/app/shared/payment-detail.model';
 import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 
 @Component({
@@ -11,9 +14,24 @@ export class PaymentDetailFormComponent implements OnInit {
 
   //Inyectamos el servicio al constructor para poder
   //hacer uso del modelo en el form.
-  constructor(public service:PaymentDetailService) { }
+  constructor(public service:PaymentDetailService,
+    private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form:NgForm){
+    this.service.postPaymentDetail().subscribe(
+      res => {
+        this.resetform(form);
+        this.toastr.success('Submited Succesfully','Payment Detail Register')
+      },
+      err => {console.log(err);}
+    );
+  }
+
+  resetform(form:NgForm){
+    form.form.reset();
+    this.service.formData = new PaymentDetail();
+  }
 }
--- src/app/shared/payment-detail.service.ts
+++ src/app/shared/payment-detail.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import { PaymentDetail } from './payment-detail.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   //inicialisamos el modelo para poder hacer uso de
   //el en el proyecto.
   formData:PaymentDetail = new PaymentDetail();
+  list: PaymentDetail[];
+
+  readonly baseUrl ='https://localhost:44349/api/PaymenDetails'
+  
+  postPaymentDetail(){
+   return this.http.post(this.baseUrl,this.formData);
+  }
+
+  refreshList(){
+    this.http.get(this.baseUrl)
+    .toPromise()
+    .then(res => this.list = res as PaymentDetail[]);
+  }
 }
